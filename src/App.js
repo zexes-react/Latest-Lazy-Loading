e325056988e5b7b0155c993b18e0721f24f1c72a
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-10-29 23:38:51 +0100

    Lazy loading. closes #1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
 import { BrowserRouter, Route, NavLink } from 'react-router-dom';
 
-import Posts from './containers/Posts';
 import User from './containers/User';
 import Welcome from './containers/Welcome';
 
+// import Posts from './containers/Posts';
+const Posts = React.lazy(() => import('./containers/Posts'));
+
 class App extends Component {
+
+
+
   render() {
     return (
+
       <BrowserRouter>
-        <React.Fragment>
+        <React.Fragment>{/* same as aux*/}
           <nav>
             <NavLink to="/user">User Page</NavLink> |&nbsp;
             <NavLink to="/posts">Posts Page</NavLink>
           </nav>
           <Route path="/" component={Welcome} exact />
           <Route path="/user" component={User} />
-          <Route path="/posts" component={Posts} />
+          <Route path="/posts" render={() => (
+              <Suspense fallback={<div>Loading...</div>}>{/*we can use a spinner here too*/}
+                <Posts/>
+              </Suspense>
+          )} />
         </React.Fragment>
       </BrowserRouter>
     );
